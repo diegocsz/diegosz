a7a25fd8f27133c9c50c754e1ef4ef9f881e55c9
diegocsalcedoz <diegocsalcedoz@gmail.com>
2024-02-19 12:19:16 -0500

    AAAAA

--- routes/index.tsx
+++ routes/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <p class="textnormal">
           Try updating this message in the
         </p>
-        <a href="https://github.com/island">
+        <a href="https://github.com/diegoisland">
           <img src="/static/githubon.png" alt="Github" srcset="" />
         </a>
         <a href="https://x.com/diegosunflower">
